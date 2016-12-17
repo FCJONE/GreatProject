4d839c65ab5ac18242a1d0a8d1fc2cab0063ccdd
User <User@DELL>
2016-12-17 13:54:37 +0700

    temporary commit

---
+++ OLEG!/OLEG!/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OLEG_.MvcApplication" Language="C#" %>
---
+++ OLEG!/OLEG!/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OLEG_
+{
+    public class User
+    {
+        private short ID { get; set; }
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private string MidName { get; set; }
+        private bool Sex { get; set; }
+        private string EMail { get; set; }
+        private string Hash { get; set; }
+    }
+}
